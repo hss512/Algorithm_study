bb1e44306773c24256af24d141f2fde30cac2505
Seong su Ha <73016759+hss512@users.noreply.github.com>
2023-03-18 18:46:23 +0900

    알고리즘 공부

--- src/baekjoon/greedy/N_5585.java
+++ src/baekjoon/greedy/N_5585.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class N_5585 {
     public static void main(String[] args) {
  
-	      Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
  
-  	    int change = 1000 - sc.nextInt();
+  	int change = 1000 - sc.nextInt();
          
         int[] coins = {500, 100, 50, 10, 5, 1};
 
-	      int count = 0;
+	int count = 0;
         
         for(int coin : coins){
             if(change >= coin){
@@ -18,6 +18,6 @@ public class N_5585 {
             }
         }
       
-	      System.out.print(count);
+	System.out.print(count);
     }
 }
