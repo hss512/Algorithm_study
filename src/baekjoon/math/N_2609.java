7e17e348d4ce6b2ab402a9d3c3282d055e74da35
Seong su Ha <73016759+hss512@users.noreply.github.com>
2023-05-08 18:34:31 +0900

    알고리즘 공부
    유클리드 호제법 (Euclidean algorithm)

---
+++ src/baekjoon/math/N_2609.java
@@ -0,0 +1,27 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+ 
+public class N_2609 {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+		
+        int a = sc.nextInt();
+    
+        int b = sc.nextInt();
+ 
+        int d = gcd(a, b);	
+ 
+        System.out.println(d);
+      
+        System.out.println(a * b / d);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
