7a6b49a8416b7712ad2ba4d62e980ecc3d1dc3cb
Seong su Ha <73016759+hss512@users.noreply.github.com>
2023-05-15 21:02:23 +0900

    알고리즘 공부

---
+++ src/baekjoon/math/N_2163.java
@@ -0,0 +1,16 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+
+public class N_2163 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int m = sc.nextInt();
+		
+        System.out.println(m * n - 1);
+    }
+}
