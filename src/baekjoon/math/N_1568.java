8af40a95bf7330571fe5a83b8cd2e6ec573117ca
Seong su Ha <73016759+hss512@users.noreply.github.com>
2023-05-26 14:03:01 +0900

    알고리즘 공부

---
+++ src/baekjoon/math/N_1568.java
@@ -0,0 +1,26 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+
+public class N_1568 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int K = 1;
+        
+        int count = 0;
+
+        while (N>0) {
+            if(N < K)
+                K = 1;
+            N -= K;
+            count++;
+            K++;
+        }
+        
+        System.out.println(count);
+    }
+}
