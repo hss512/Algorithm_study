fd56df148a1a75fd69588d5729bb62f965445987
Seong su Ha <73016759+hss512@users.noreply.github.com>
2023-06-01 19:11:06 +0900

    알고리즘 공부

---
+++ src/baekjoon/sort/N_9076.java
@@ -0,0 +1,26 @@
+package baekjoon.sort;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class N_9076 {
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        
+        int[] score = new int[5];
+        
+        for(int i=0;i<num;i++){
+        	for(int j=0;j<score.length;j++){
+            	score[j] = sc.nextInt();
+            }
+        	Arrays.sort(score);
+        	if(score[3] - score[1] >= 4){
+        		System.out.println("KIN");
+        	}else
+        		System.out.println(score[1] + score[2] + score[3]);
+        }
+    }
+}
